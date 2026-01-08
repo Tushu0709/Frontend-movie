e49f3e66a4b9177561733a966b37be754711d7d3
Tushar Chavan <https://github.com/Tushu0709/E-Book-Website.git>
2026-01-08 16:37:31 +0530

    changes

--- src/lib/timeFormat.js
+++ src/lib/timeFormat.js
@@ -1,7 +1,15 @@
-const timeFormat = (minutes) => {
-  const hours = Math.floor(minutes / 60);
-  const mintuesRemainder = minutes % 60;
-  return `${hours}h ${mintuesRemainder}m`;
-};
+// const timeFormat = (minutes) => {
+//   const hours = Math.floor(minutes / 60);
+//   const mintuesRemainder = minutes % 60;
+//   return `${hours}h ${mintuesRemainder}m`;
+// };
 
-export default timeFormat;
+// export default timeFormat;
+
+
+export default function timeformat(minutes) {
+  if (!minutes) return "0h 0m";
+  const h = Math.floor(minutes / 60);
+  const m = minutes % 60;
+  return `${h}h ${m}m`;
+}
