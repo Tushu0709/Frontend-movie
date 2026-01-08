f90aebe61af757bf43cbad4f0352c9fec96cada9
Tushar Chavan <https://github.com/Tushu0709/E-Book-Website.git>
2026-01-08 18:09:48 +0530

    Refactor admin login route to /adminlogin

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
 
 
         <Route path="/login" element={<Login />} />
-        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/adminlogin" element={<AdminLogin />} />
         <Route path="/register" element={<Register />} />
 
         {/* Protected Admin Routes */}
