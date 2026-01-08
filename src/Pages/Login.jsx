3c5eb83010ee679b2f060049b175465a90002748
Tushar Chavan <https://github.com/Tushu0709/E-Book-Website.git>
2026-01-08 22:53:51 +0530

    Changes

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, formData);
+            const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/login`, formData);
             if (data.success) {
                 loginUser(data.token, data.user);
                 toast.success('Login Successful');
