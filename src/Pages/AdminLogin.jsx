ab6b7cc746e3e073c258d614200d358ff7b7afb0
Tushar Chavan <https://github.com/Tushu0709/E-Book-Website.git>
2026-01-09 11:47:56 +0530

    Admin Update

--- src/Pages/AdminLogin.jsx
+++ src/Pages/AdminLogin.jsx
@@ -16,7 +16,7 @@ const AdminLogin = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/admin/login`,
+        `${import.meta.env.VITE_API_URL}/api/auth/login`,
         formData
       );
       if (data.success) {
